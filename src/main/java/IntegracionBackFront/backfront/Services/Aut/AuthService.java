16dac83af8be23703b55e45cb334dd6547ea035f
Rivalsito008 <iabarreraescalante@gmail.com>
2025-09-04 15:45:40 -0600

    push

---
+++ src/main/java/IntegracionBackFront/backfront/Services/Aut/AuthService.java
@@ -0,0 +1,35 @@
+package IntegracionBackFront.backfront.Services.Aut;
+
+import IntegracionBackFront.backfront.Config.Argon2.Argon2Password;
+import IntegracionBackFront.backfront.Entities.Users.UserEntity;
+import IntegracionBackFront.backfront.Repositories.Users.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class AuthService {
+
+    @Autowired
+    private UserRepository repo;
+
+    public Boolean Login(String correo, String contrasena){
+        Argon2Password objHash = new Argon2Password();
+        Optional<UserEntity> list = repo.findByCorreo(correo).stream().findFirst();
+        if (list.isPresent()){
+            UserEntity usuario = list.get();
+            String nombreTipoUsuario = usuario.getTipoUsuario().getNombreTipo();
+            System.out.println("Usuario ID encontrado: " + usuario.getId() + ", email: " + usuario.getCorreo() + ", rol: " + nombreTipoUsuario);
+            String HashDB = usuario.getContrasena();
+            boolean verificado = objHash.VerifyPassword(HashDB, contrasena);
+            return verificado;
+        }
+        return false;
+    }
+
+    public Optional<UserEntity> obtenerUsuario(String email){
+        Optional<UserEntity> userOpt = repo.findByCorreo(email);
+        return (userOpt != null ) ? userOpt : null;
+    }
+}
